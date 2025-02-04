98c292397ededb52dadded323bd139237e195ac0
Mathesh <mathesh1907@gmail.com>
2025-02-04 17:45:00 +0530

    HEHE

---
+++ src/tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  safelist: [
+    'autoBlur', // Your custom class
+    'blur-animation', // Keyframe animation name
+  ],
+  theme: {
+    extend: {
+      animation: {
+        'blur-animation': 'blur-animation 0.1s infinate'
+         // Extend Tailwind animations
+      },
+      keyframes: {
+        'blur-animation': {
+          '0%': { filter: 'blur(40px)' },
+          '50%, 53%': { filter: 'blur(0px)' },
+          '100%': { filter: 'blur(40px)' },
+        },
+      },
+      fontFamily: {
+        'tsan': ['Noto Serif', 'serif'],
+         bely: ['Bely Display', 'sans-serif'], 
+         Satoshi: ['Satoshi Display', 'sans-serif'], 
+          // Extend font families
+      },
+    },
+  },
+  plugins: [],
+};
